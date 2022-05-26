2d0d681e827e429f4d4b9ab15c6648126c57d493
tsnanh-napa <anh.tran@napaglobal.com>
2022-05-26 13:22:02 +0700

    feature: Add more attributes

--- alertdialog-ktx/src/main/java/dev/tsnanh/alertdialog/ktx/AlertDialogKtx.kt
+++ alertdialog-ktx/src/main/java/dev/tsnanh/alertdialog/ktx/AlertDialogKtx.kt
@@ -1,8 +1,14 @@
 package dev.tsnanh.alertdialog.ktx
 
 import android.content.Context
+import android.content.DialogInterface
 import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import androidx.annotation.AttrRes
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dev.tsnanh.alertdialog.ktx.models.BackgroundInsetValues
 
 data class AlertDialogKtx(
     val background: Drawable? = null,
@@ -14,9 +20,18 @@ data class AlertDialogKtx(
     val positiveButtonScope: DialogButtonScope? = null,
     val negativeButtonScope: DialogButtonScope? = null,
     val neutralButtonScope: DialogButtonScope? = null,
+    val backgroundInsetValues: BackgroundInsetValues = BackgroundInsetValues(),
+    val adapter: ListAdapter? = null,
+    val onItemClickListener: ((dialog: DialogInterface, which: Int) -> Unit)? = null,
+    val customTitle: View? = null,
+    @AttrRes val iconAttribute: Int? = null,
+    val onCancelListener: ((DialogInterface) -> Unit)? = null,
 )
 
 fun AlertDialogKtx.createAlertDialog(ctx: Context) = with(this) {
+    assert(adapter != null && onItemClickListener != null) {
+        "Hehe"
+    }
     MaterialAlertDialogBuilder(ctx)
         .setTitle(title)
         .setMessage(message)
@@ -32,9 +47,23 @@ fun AlertDialogKtx.createAlertDialog(ctx: Context) = with(this) {
             neutralButtonScope?.onClick?.invoke(d, w)
         }
         .setNeutralButtonIcon(neutralButtonScope?.icon)
+        .setBackgroundInsetStart(backgroundInsetValues.start)
+        .setBackgroundInsetEnd(backgroundInsetValues.end)
+        .setBackgroundInsetTop(backgroundInsetValues.top)
+        .setBackgroundInsetBottom(backgroundInsetValues.bottom)
+        .setAdapter(adapter) { d, w ->
+            onItemClickListener?.invoke(d, w)
+        }
+        .setCustomTitle(customTitle)
+        .setOnCancelListener { d ->
+            onCancelListener?.invoke(d)
+        }
         .apply {
             this@with.background?.let {
                 this.background = it
+            }
+            this@with.iconAttribute?.let {
+                setIconAttribute(iconAttribute)
             }
         }
         .create()
---
+++ alertdialog-ktx/src/main/java/dev/tsnanh/alertdialog/ktx/models/BackgroundInsetValues.kt
@@ -0,0 +1,8 @@
+package dev.tsnanh.alertdialog.ktx.models
+
+data class BackgroundInsetValues(
+    val top: Int = 0,
+    val bottom: Int = 0,
+    val start: Int = 0,
+    val end: Int = 0,
+)
